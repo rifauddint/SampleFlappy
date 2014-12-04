7d90be0ccec507269acb396010a1239586288b0b
Rifauddin Tsalitsy <munchlax.b@gmail.com>
2014-12-04 10:09:46 +0700

    Update Kalah

--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -18,7 +18,8 @@ public class MoveScript : MonoBehaviour
 		{
 				rigidbody2D.velocity = new Vector2 (speedX, 0);
 				if (gameObject.transform.position.x < -4) {
-						gameObject.transform.position = new Vector3 (4, 0, 10);
+						float randomY = Random.Range (-2, 2);
+						gameObject.transform.position = new Vector3 (4, randomY, 10);
 				
 				}
 		}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKey (KeyCode.Space)) {
+						rigidbody2D.velocity = new Vector2 (0, 5);
+			
+				}
+		}
+		
+		void OnCollisionEnter2D ()
+		{
+				var movePipes = GameObject.FindObjectsOfType<MoveScript> ();
+			
+				
+				foreach (MoveScript pipe in movePipes) {
+			
+						pipe.speedX = 0;
+						rigidbody2D.velocity = new Vector2 (0, -5);
+			
+				}
+		
+		}
+
+}
+
+
